4854e712c6c905e5d49fd1f31d83c0b4a2520fb6
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-17 17:42:38 +0000

    Object Check

---
+++ Non_Primitive.js
@@ -0,0 +1,14 @@
+// Non- Primitive Data-types
+// Array
+let arr = [12,34,56,92]
+// Object
+let arrayObject = {
+    1:"Hello",
+    2:"Moto",
+    4:"Divine",
+    5:arr
+}
+let array = [arrayObject,34,56,78,90];
+console.log(typeof array)
+console.log(array)
+console.log(arrayObject)
