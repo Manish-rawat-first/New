1bd6ec24263995bb04a182c47a54b1b43ae7abd9
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-17 17:32:51 +0000

    Primitive data types checks

---
+++ Data.js
@@ -0,0 +1,15 @@
+let a = "Hello"
+//Primitive Datatypes.
+console.log(typeof a);
+a = 5;
+console.log(typeof a);
+a = true;
+console.log(typeof a);
+a = null;
+console.log(typeof a);
+a = undefined;
+console.log(a);
+let b = 453429302328n;
+console.log(typeof b);
+a = Symbol(123)
+console.log(typeof a)
