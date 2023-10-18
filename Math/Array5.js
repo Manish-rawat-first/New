0fcd4e3258e67f3d2733146a9c99f0211d5740e6
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-18 12:58:44 +0000

    Array5

---
+++ Math/Array5.js
@@ -0,0 +1,11 @@
+const s = [1,23,14,56,78,90,21]
+console.log(Array.isArray(s))
+console.log(s.join())
+console.log(Array.from("Manish"))
+console.log(Array.from({a:"Manish"}))
+
+let a = "23";
+let b = 90;
+let c = true;
+let m = Array.of(a,b,c)
+console.log(m)
