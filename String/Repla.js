a4ee6cd04575d64ca135d2ba9aa63b2575518f0f
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-18 04:33:13 +0000

    Substring

--- String/Repla.js
+++ String/Repla.js
@@ -1,3 +1,6 @@
 let rep = "Abcd-ef-gh-i-jk-lm-n -o";
 console.log(rep.replaceAll('-',' '));
 console.log(rep.replace('-','G'))
+
+let s = "Rohan"
+console.log(s.substring(0,4))//Substring start and end index index not include.
