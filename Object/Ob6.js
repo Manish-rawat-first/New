5988ba5dac109503502c51318e92ac5f9b90af21
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-18 15:03:57 +0000

    Ok

--- Object/Ob6.js
+++ Object/Ob6.js
@@ -3,4 +3,10 @@ let ob = {
     2:"Motu",
     3:"Chotu"
 }
-console.log(ob)
+let k = Object.keys(ob)
+let m = Object.values(ob)
+console.log(`${k},  ${m}`)
+
+console.log(Object.entries(ob))
+
+console.log(ob.hasOwnProperty(3))
