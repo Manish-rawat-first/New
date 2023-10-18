76fe8555de901542bf00929296c68c852746ea02
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-18 17:21:40 +0000

    Scope

---
+++ Function/Scope.js
@@ -0,0 +1,12 @@
+var a = 56;
+//var a = 21;// No declare because you have already declare variable Var.
+console.log(a)
+let ac= 45;
+const ab = 9
+{
+   const ab = 97
+   ac = 90
+   console.log(ac)
+}
+console.log(ac)
+console.log(ab)
