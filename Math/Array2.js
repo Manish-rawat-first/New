16e21148937224ff718d09e9939e7ccbbd949ac9
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-18 08:54:51 +0000

    Arr

--- Math/Array2.js
+++ Math/Array2.js
@@ -1,4 +1,4 @@
-let arr = [23,45,67,89,0];
+const arr = [23,45,67,89,0];
 console.log('A',arr)
 console.log('C',arr.slice(0,3))// Actual Peace not separate.
 console.log('B',arr.splice(0,3))// Actual Peace Separate
