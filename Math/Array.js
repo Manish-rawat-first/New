4c45eef892ce049b8a030533e2f1f09980d024e7
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-18 08:22:00 +0000

    Array Practice

---
+++ Math/Array.js
@@ -0,0 +1,16 @@
+let myarr = [0,1,2,3,4,5,6,7,8];
+console.log(myarr);
+
+
+myarr.push(9)
+myarr.push(11)
+console.log(myarr)
+
+myarr.pop()
+console.log(myarr)
+
+myarr.unshift(12)
+console.log(myarr)
+
+console.log(myarr.shift())
+
