2fd8a369b7e11ca472cc66eb07a98926052e9f2c
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-18 13:09:13 +0000

    Object

---
+++ Object/Ob1.js
@@ -0,0 +1,19 @@
+const sy = Symbol("Mykey");
+const s = {
+  user: "manishrawat4876@gmail.com",
+  password: "Manish$789",
+  pincode: 471001,
+  dob: 20042002,
+  mob: 7879775525,
+  Name:"Manish Rawat",
+  Branch:"CSE",
+  [sy]:"Rohan"
+};
+console.log(s["dob"])
+console.log(s["Name"])
+console.log(s["pincode"])
+console.log(s["Branch"])
+console.log(s)
+Object.freeze(s);// Freeze Object Cannot change value.
+s.Name = "Mohan";
+console.log(s)
