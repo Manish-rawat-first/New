22fa06002d9596981f9b3f716a238633214dd7c0
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-19 14:14:02 +0000

    Switch

---
+++ IIFE/Switch.js
@@ -0,0 +1,23 @@
+const num = 4;
+switch (num) {
+  case 1:
+    console.log("Papa");
+    break;
+  case 2:
+    console.log("Mummy");
+    break;
+  case 3:
+    console.log("Shalu didi");
+    break;
+  case 4:
+    console.log("Mayank Bhaiya");
+    break;
+  case 5:
+    console.log("Sakshi didi");
+    break;
+  case 5:
+    console.log("Manish Rawat");
+    break;
+  default:
+    console.log("Unknown");
+}
