d637631580ce317cd53da20228cf9bb9a07f172d
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-19 14:10:10 +0000

    Conditional

---
+++ IIFE/Condi.js
@@ -0,0 +1,22 @@
+let day = "Wednesday";
+if(day=="Monday"){
+    console.log("Monday");
+}
+else if(day=="Tuesday"){
+    console.log("Tuesday");
+}
+else if(day=="Wednesday"){
+    console.log("Wednesday");
+}
+else if(day=="Thrusday"){
+    console.log("Thrusday");
+}
+else if(day=="Friday"){
+    console.log("Friday");
+}
+else if(day=="Saturday"){
+    console.log("Satuday");
+}
+else{
+    console.log("Sunday");
+}
