38add977d55a17dd4002ec2e5deb5c4964477b24
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-19 19:13:17 +0000

    Array

---
+++ Map/Array.js
@@ -0,0 +1,14 @@
+let array = [{
+    language:"Java",
+    Extension:".java"
+},
+{
+    language:"JavaScript",
+    Extension:".js"
+},
+{
+    language:"Python",
+    Extension:".py"
+}
+]
+array.forEach(item=>console.log(`language: ${item.language} Extension: ${item.Extension}`));
