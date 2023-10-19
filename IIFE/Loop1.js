168c8a0bd2ca55cafd65cd3fd1962618c4d9cbba
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-19 18:01:26 +0000

    Loop

--- IIFE/Loop1.js
+++ IIFE/Loop1.js
@@ -1,7 +1,15 @@
-const s = [1,2,3,4,5,6];
-for(let i = 1;i<s.length;i++){
-    console.log(s[i]);
-    if(i==3){
-        break;
-    }
+const s = [1, 2, 3, 4, 5, 6];
+console.log("For Loop");
+for (let i = 1; i < s.length; i++) {
+  console.log(s[i]);
+  if (i == 3) {
+    break;
+  }
+}
+console.log("While Loop");
+// while loop
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i += 2;
 }
