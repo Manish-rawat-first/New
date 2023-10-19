7716e45f10ed570754dc6272d87db5fefad99e5b
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-19 04:29:03 +0000

    IIFI

---
+++ IIFE/Da.js
@@ -0,0 +1,11 @@
+//Immediately Invoked Function Expression.
+//Global Scope Pollution prevent is used.
+//Named IIFI and Unnamed IIFI.
+((motu)=>console.log("hello:",motu)
+)("Gada");
+
+((motu)=>console.log("hello:",motu)
+)("Rad");
+
+
+ 
