1a9af880afbed3b406f4d6fdb5fbf46a496a2b39
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-19 03:56:54 +0000

    String

---
+++ String/Method.js
@@ -0,0 +1,6 @@
+const str = "Raja Moli Sahab";
+console.log(str.charCodeAt(3))
+console.log(str.indexOf('z'))
+console.log(str.match('Raja'))
+console.log(str.repeat(3))
+console.log(str.localeCompare("King Khan SuperStar"))
