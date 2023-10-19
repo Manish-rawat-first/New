2635ada7ad4fd7e25fb93752e5ba2dc306330f66
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-19 18:21:49 +0000

    Map

--- Map/Trial.js
+++ Map/Trial.js
@@ -3,3 +3,13 @@ map.set("name","Manish");
 map.set("lname","Rawat");
 map.set("name","Tarun");
 console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key,' ',value)
+    
+}
+
+for (const key of map) {
+    console.log(key)
+    
+}
