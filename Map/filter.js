fb9e4142393b6f4ba8fc66434ff944d8432bf074
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-20 03:11:31 +0000

    filter.js

---
+++ Map/filter.js
@@ -0,0 +1,6 @@
+let array = [12,34,56,78,90];
+const val = array.filter(item=>item>34);
+console.log(val);
+// Special condiiton follow.
+const repo = array.filter(item=>item!=56);
+console.log(repo)
