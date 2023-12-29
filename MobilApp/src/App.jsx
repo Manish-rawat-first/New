8bde31e71ce3f5cee33613d3a0ca2e4d7f49fe4c
Manish Rawat <128154980+Manish-rawat-first@users.noreply.github.com>
2023-12-29 19:15:47 +0000

    Value

--- MobilApp/src/App.jsx
+++ MobilApp/src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-
+  let [counter,setState] = useState(15);
+function addValue(){
+  counter = counter+1;
+  setState(counter)
+}
+function DecValue(){
+  counter = counter-1;
+  setState(counter)
+}
   return (
     <>
-      
-     
+    <h1>Counter App : {counter}</h1>
+    <h2>Increase V/S Decrease App</h2>
+    <button onClick={addValue}>Add Value {counter}</button>
+    <br/>
+    <button onClick={DecValue}>Decrease Value {counter}</button>
     </>
   )
 }
