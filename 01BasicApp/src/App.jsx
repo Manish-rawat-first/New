cc477613fad4f185c283c91527e0140a54f23327
Manish Rawat <128154980+Manish-rawat-first@users.noreply.github.com>
2023-12-29 18:24:17 +0000

    Commit

--- 01BasicApp/src/App.jsx
+++ 01BasicApp/src/App.jsx
@@ -1,10 +1,12 @@
 import Chai from './Chai';
 function App() {
+  const username = "chaiaurcode";
 
   return (
     // Fragment term.
+    //{username} Evaluated Expression.
     <>
-    <h1><i>Chai aur react with vite || Manish Rawat</i></h1>
+    <h1><i>Chai aur react with vite || Manish Rawat  {username}</i></h1>
     <Chai/>
     </>
   )
--- 01BasicApp/src/main.jsx
+++ 01BasicApp/src/main.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-function MyApp(){
-  return(
-    <div>
-      <h1>Custom App !</h1>
-    </div>
-  )
-}
+// function MyApp(){
+//   return(
+//     <div>
+//       <h1>Custom App !</h1>
+//     </div>
+//   )
+// }
+// const reactElement = React.createElement(
+//   'a',
+//   {href:"https://google.com",target:"_blank"},
+//   'Click me to Visit google'
+// );
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MyApp/>
+    {/* reactElement */}
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
