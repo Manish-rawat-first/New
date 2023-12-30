e8a485a56b734b9eb5a995d421803b5389cedbfa
Manish Rawat <128154980+Manish-rawat-first@users.noreply.github.com>
2023-12-30 12:35:18 +0000

    changes

--- MindApp/src/App.jsx
+++ MindApp/src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-   <h1>Hello JS</h1>
+    <>
+   <h1 className='bg-green-400 text-white p-4 rounded-xl'>Hello JS</h1>
+   </>
   )
 }
 
